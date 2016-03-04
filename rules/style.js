0121e9a41209d78f00e8086f39088ba9c7df246d
Neil Kistner <neil.kistner@gmail.com>
2016-03-03 18:54:49 -0600

    Add `no-multiple-empty-lines` with `maxBOF: 0`
    This rule configured this way will disallow empty lines at the beginning
    of files.

--- rules/style.js
+++ rules/style.js
@@ -14,6 +14,9 @@ module.exports = {
     // allow/disallow an empty newline after var statement
     // https://github.com/eslint/eslint/blob/master/docs/rules/newline-after-var.md
     'newline-after-var': [2, 'always'],
+    // disallow multiple empty lines and only one newline at the end
+    // https://github.com/eslint/eslint/blob/master/docs/rules/no-multiple-empty-lines.md
+    'no-multiple-empty-lines': [2, { 'max': 2, 'maxBOF': 0, 'maxEOF': 1 }],
     // require or disallow use of semicolons instead of ASI
     // I use Babel, it takes care of this for me
     'semi': [2, 'never'],
