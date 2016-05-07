4d4c4477b1e094227c81f5498e48209fc4a61d26
Neil Kistner <neil.kistner@gmail.com>
2016-05-06 22:11:14 -0500

    Fixing lint rules

--- rules/es6.js
+++ rules/es6.js
@@ -15,19 +15,16 @@ module.exports = {
     'import/default': 2,
     // disallow non-import statements appearing before import statements
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/imports-first.md
-    'import/imports-first': 2,
+    'import/imports-first': [2, ''],
     // ensure named imports coupled with named exports
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/named.md#when-not-to-use-it
     'import/named': 2,
-    // ensure new line after last import/require in group
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/newline-after-import.md
-    'import/newline-after-import': 2,
     // disallow duplicate imports
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-duplicates.md
     'import/no-duplicates': 2,
     // Forbid the use of extraneous packages
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-extraneous-dependencies.md
-    'import/no-extraneous-dependencies': 2,
+    'import/no-extraneous-dependencies': [2, { 'devDependencies': true }],
     // disallow arrow functions where they could be confused with comparisons
     // http://eslint.org/docs/rules/no-confusing-arrow
     'no-confusing-arrow': 0,
