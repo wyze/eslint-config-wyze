9925fc038f5faea80e31436b05aa2f79fb1b619d
Neil Kistner <neil.kistner@gmail.com>
2016-11-01 14:58:49 -0500

    Remove rules implemented in Airbnb

--- rules/style.js
+++ rules/style.js
@@ -23,17 +23,6 @@ module.exports = {
     // require or disallow use of semicolons instead of ASI
     // I use Babel, it takes care of this for me
     'semi': [2, 'never'],
-    // space around the keywords.
-    // http://github.com/eslint/eslint/blob/master/docs/rules/keyword-spacing.md
-    'keyword-spacing': [2, {
-      'before': true,
-      'after': true,
-      'overrides': {
-        'case': { 'after': true },
-        'return': { 'after': true },
-        'throw': { 'after': true }
-      }
-    }],
     // require or disallow spaces inside parentheses
     // disabled, because this rule is strange
     // TODO: write and implement a better rule
