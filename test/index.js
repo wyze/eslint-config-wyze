c52a6e1eb92bf034aa11f8dd5673125513a85102
Neil Kistner <neil.kistner@gmail.com>
2016-11-10 22:32:25 -0600

    Fix test for default config

--- test/index.js
+++ test/index.js
@@ -3,7 +3,9 @@ import test from 'ava'
 
 test('extends files', t => {
   const expected = [
-    'airbnb',
+    'airbnb-base',
+    'airbnb-base/rules/strict',
+    'airbnb/rules/react',
     'wyze/base',
     'wyze/rules/react',
     'wyze/rules/jsx-a11y',
