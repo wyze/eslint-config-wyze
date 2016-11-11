e13d9eb8622e096e5e6138f271ace86d353f85e0
Neil Kistner <neil.kistner@gmail.com>
2016-11-10 22:31:23 -0600

    Properly override rules for `jsx-a11y`

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 module.exports = {
   extends: [
-    'airbnb',
+    // Explicitly not load `airbnb/rules/react-a11y` until it uses
+    // eslint-plugin-jsx-a11y@^3.0.0
+    'airbnb-base',
+    'airbnb-base/rules/strict',
+    'airbnb/rules/react',
     'wyze/base',
     'wyze/rules/react',
     'wyze/rules/jsx-a11y',
