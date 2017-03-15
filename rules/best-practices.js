96569b19a8a0ac2dfc7e036f718df6d9d66a022d
Neil Kistner <neil.kistner@gmail.com>
2017-03-15 13:40:28 -0500

    Remove `id-blacklist` rule and use AirBnb's setting

--- rules/best-practices.js
+++ rules/best-practices.js
@@ -3,15 +3,6 @@ module.exports = {
     // require the use of === and !==
     // http://eslint.org/docs/rules/eqeqeq
     eqeqeq: 'error',
-    // Blacklist certain identifiers to prevent them being used
-    // http://eslint.org/docs/rules/id-blacklist
-    'id-blacklist': [ 'error',
-      'callback',
-      'cb',
-      'data',
-      'e',
-      'err',
-    ],
     // disallow unmodified conditions of loops
     // http://eslint.org/docs/rules/no-unmodified-loop-condition
     'no-unmodified-loop-condition': 'error',
